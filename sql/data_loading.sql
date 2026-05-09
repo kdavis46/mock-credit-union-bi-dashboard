9832639b7c2d7a651078ff14d22c0c24ded7e5f5
kdavis46 <kdavis4456@gmail.com>
2026-05-09 16:33:42 -0400

    SQL script for loading data from CSV files

---
+++ sql/data_loading.sql
@@ -0,0 +1,7 @@
+\copy members FROM 'members.csv' CSV HEADER;
+
+\copy accounts FROM 'accounts.csv' CSV HEADER;
+
+\copy transactions FROM 'transactions.csv' CSV HEADER;
+
+\copy loans FROM 'loans.csv' CSV HEADER;
